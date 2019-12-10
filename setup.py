cc4425a9403de74767ab6a3baf623105e0d71e57
José Sánchez <jose@ssanchez.net>
2019-12-10 15:52:23 +0000

    Initial commit
    Creating empty project to reserve the name on pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup
+
+setup(
+    name='appbuilder',
+    version='0.0.20191210.1',
+    description='Misc utilities for everything',
+    url='http://github.com/acceso/appbuilder',
+    author='Jose Sanchez',
+    license='GNU Affero General Public License v3',
+    packages=['appbuilder'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
